ecfcf8e36d71687aae52d2ab638e91d995392089
john russelle <kiddogreed@gmail.com>
2022-11-30 23:22:26 +0800

    local update from macbook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class  App extends Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      monsters :[],
+    
+    }
+    console.log("constructor");
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+componentDidMount(){
+    console.log("component did mount");
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(()=>{
+      return {monsters:users}
+    }, ()=>{
+      console.log(this.state);
+    }))
+  
+
+}
+
+
+  render() {
+    console.log("render")
+    return (
+      <div className="App">
+      <input className='search-box' type='search' placeholder='search monsters' onChange={(event)=>{
+        //console.log(event.target.value);
+        console.log(this.state.monsters.name)
+        let monsterArr = this.state.monsters
+        console.log(monsterArr)
+        //task try to update the state using array.filter
+
+      }}
+
+      />
+      {this.state.monsters.map((monster)=>{
+        return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        )
+      })}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
