432f66f36e0c8516378d7bded206b0f943f8e559
john russelle <kiddogreed@gmail.com>
2022-12-01 06:51:35 +0800

    add

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,9 @@ import { Component } from "react";
 
 class CardList extends Component {
   render () {
-    return <div>Hello this is cardlist</div>
+    return (
+    <div>Hello this is cardlist</div>
+    )
   }
 }
 
